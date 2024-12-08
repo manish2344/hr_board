0f68b301e54ccbfac44183b6488589ad3b7f6da7
manish <manishpanwar682@gmail.com>
2024-12-07 17:44:31 -1200

    first commit

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 connectDB()
-app.get('*', (req, res) => {
-    res.send('Hello from server.js!');
-  });
+// app.get('*', (req, res) => {
+//     res.send('Hello from server.js!');
+//   });
 
 app.get("/", (req, res) => {
     res.send('Hello, World!');
